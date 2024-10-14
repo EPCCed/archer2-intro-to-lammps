648dc9de08f27b2ab9631769717d7483464ea925
Rui Apóstolo <ruiapostolo@gmail.com>
2024-10-14 16:36:43 +0100

    Exercise 4 files

---
+++ exercises/4-creating-topology/topo.tcl
@@ -0,0 +1,27 @@
+package require topotools
+proc get_total_charge  { eval "vecadd [[atomselect $molid all] get charge]" }
+mol bondsrecalc all
+topo retypebonds
+topo guessangles
+topo guessdihedrals
+
+puts ""
+puts "Bond type names: "
+puts [ topo bondtypenames ]
+puts ""
+puts "Angle type names: "
+puts [ topo angletypenames ]
+puts ""
+puts "Dihedral type names: "
+puts [ topo dihedraltypenames ]
+puts ""
+puts ""
+puts [format "Number of bonds:          %s" [topo numbonds]         ]
+puts [format "Number of bonds types:    %s" [topo numbondtypes]     ]
+puts [format "Number of angles:         %s" [topo numangles]        ]
+puts [format "Number of angles types:   %s" [topo numangletypes]    ]
+puts [format "Number of dihedral:       %s" [topo numdihedrals]     ]
+puts [format "Number of dihedral types: %s" [topo numdihedraltypes] ]
+puts [format "Total charge:             %s" [get_total_charge]      ]
+puts ""
+topo writelammpsdata data.water_ethanol
---
+++ exercises/4-creating-topology/water.tcl
@@ -0,0 +1,13 @@
+set selwo [atomselect top {name WO}]
+$selwo set mass 15.999
+$selwo set type WO
+$selwo set charge -0.8340
+$selwo set element O
+$selwo set radius 1.2
+
+set selwh [atomselect top {name WH}]
+$selwh set mass 1.008
+$selwh set type WH
+$selwh set charge 0.4170
+$selwh set element H
+$selwh set radius 0.8
